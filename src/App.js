1cee863e718a1fbf197f4a727474bd05cc3a44b2
Marie Pauline <mariepsk@gmail.com>
2022-12-02 13:22:54 +0100

    Fix gh pages

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [dog, setDog] = useState(null);
-   useEffect(() => {
-      fetch('https://dog.ceo/api/breeds/image/random')
-         .then((response) => response.json())
-         .then((data) => {
-            setDog(data.message);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
+  const refresh = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+      setDog(data.message);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
+  useEffect(refresh, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Let me light up your day!</h1>
       </header>
-      <section>
+      <section className="dogs">
         <img src={dog} alt="Random dog" />
+        <button onClick={refresh}>New cuteness?</button>
       </section>
     </div>
   );
